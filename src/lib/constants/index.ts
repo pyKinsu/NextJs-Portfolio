f56eea8ce18d0a63b953f5eb10a8a64c88ec3f10
Kinsu Rajput <kissuxrajput@gmail.com>
2025-09-08 17:12:45 +0530

    Create index.ts

---
+++ src/lib/constants/index.ts
@@ -0,0 +1,207 @@
+import { FaYoutube, FaLinkedin } from "react-icons/fa";
+import {
+  RxDiscordLogo,
+  RxGithubLogo,
+  RxInstagramLogo,
+  RxTwitterLogo,
+  RxLinkedinLogo,
+} from "react-icons/rx";
+
+export const skills = [
+  {
+    skill_name: "HTML",
+    image: "html.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "CSS",
+    image: "css.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "JavaScript",
+    image: "js.png",
+    width: 65,
+    height: 65,
+  },
+  {
+    skill_name: "Tailwind CSS",
+    image: "tailwind.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "React",
+    image: "react.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Redux",
+    image: "redux.png",
+    width: 80,
+    height: 80,
+  },
+
+  {
+    skill_name: "TypeScript",
+    image: "ts.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Next.js 14",
+    image: "next.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Framer Motion",
+    image: "framer.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Node.js",
+    image: "node.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "MongoDB",
+    image: "mongodb.png",
+    width: 40,
+    height: 40,
+  },
+] as const;
+
+export const socials = [
+  {
+    name: "Instagram",
+    icon: RxInstagramLogo,
+    link: "https://instagram.com/pykinsu/",
+  },
+  {
+    name: "LinkedIn",
+    icon: FaLinkedin,
+    link: "https://linkedin.com/in/pykinsu/",
+  },
+  {
+    name: "Twitter",
+    icon: RxTwitterLogo,
+    link: "https://twitter.com/pykinsu/",
+  },
+] as const;
+
+export const frontend_skills = [
+  {
+    skill_name: "HTML",
+    image: "html.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "CSS",
+    image: "css.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "JavaScript",
+    image: "js.png",
+    width: 65,
+    height: 65,
+  },
+  {
+    skill_name: "Tailwind CSS",
+    image: "tailwind.png",
+    width: 80,
+    height: 80,
+  },
+
+  {
+    skill_name: "React",
+    image: "react.png",
+    width: 80,
+    height: 80,
+  },
+
+
+  {
+    skill_name: "Next.js 14",
+    image: "next.png",
+    width: 80,
+    height: 80,
+  },
+] as const;
+
+export const backend_skills = [
+  {
+    skill_name: "Node.js",
+    image: "node.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Express.js",
+    image: "express.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "MongoDB",
+    image: "mongodb.png",
+    width: 40,
+    height: 40,
+  },
+
+
+] as const;
+
+
+export const projects = [
+  {
+    title: "ReelSaver – Instagram Reel Downloader & Api",
+    
+    image: "/projects/project1.jpg",
+    link: "https://reelsaver.vercel.app/",
+  },
+  {
+    title: "My Portfolio - HTML & CSS based portfolio ",
+
+    image: "/projects/project2.jpg",
+    link: "https://kinsu.onrender.com",
+  },
+  {
+    title: "EmojiSpot Get Emoji Pngs in different colors",
+   
+    image: "/projects/project3.jpg",
+    link: "https://emojispot.onrender.com/",
+  },
+] as const;
+
+export const navlinks = [
+  {
+    title: "About me",
+    link: "#about",
+  },
+  {
+    title: "Skills",
+    link: "#skills",
+  },
+  {
+    title: "Projects",
+    link: "#projects",
+  },
+  {
+    title: "Contact",
+    link: "#contact",
+  },
+  {
+    title: "Source Code",
+    link: "https://github.com/pyKinsu/NextJs-Portfolio",
+  },
+  
+] as const;
