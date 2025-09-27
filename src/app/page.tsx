780e2741373bab23167bcb5c8d60fd557e93d1b1
Kinsu Rajput <kissuxrajput@gmail.com>
2025-09-27 21:23:06 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { Hero } from "@/components/sections/Hero";
 import { Projects } from "@/components/sections/Projects";
 import { Skills } from "@/components/sections/Skills";
